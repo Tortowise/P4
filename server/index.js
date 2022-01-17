3493b1d05db8800166f1a8f003241a78769730ad
Egor Dubeshko <genkypoem@gmail.com>
2022-01-17 14:47:12 +0300

    Created server and connected it to a PostrgeSQL database

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+const sequelize = require('./db.js');
+const app = express();
+
+
+const start = async  () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
